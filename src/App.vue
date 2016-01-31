f5b2772966bc90c4e135c3b9c5afbf30f28ee9e7
stonexer <shitianxin@gmail.com>
2016-01-31 17:56:11 +0800

    vue loader

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+    <div id="app">
+        <div class="item" v-for="col in pic[0]" track-by="$index">
+            <div class="column" 
+                v-bind:class="{ 'beSwapedColumn': $index == pic[1], 'swapColumn': $index == pic[2] }"
+                v-bind:style="{height: col * 10 + 'px'}">
+             </div>
+             <div class="num">
+                {{col}}
+             </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { quickSort } from './QuickSort'
+
+var data = {
+    pic: [1,2,31,3,12,21,23,19,6,5,17,4,2,8,5]
+};
+
+export default {
+    data () {
+      return data;
+    }
+};
+
+var results = quickSort(data.pic);
+
+for(var x in results) {
+    (function(x) {
+        setTimeout(function() {
+            data.pic = results[x];
+        }, x * 500);
+    })(x);
+}
+
+</script>
+
+<style>
+.item {
+    display: inline-block;
+    width: 20px;
+    height: 10px;
+    margin-right: 5px;
+}
+.column {
+    background-color: green;
+}
+.item .num {
+    text-align: center;
+}
+.item .beSwapedColumn {
+    background-color: red;
+}
+.item .swapColumn {
+    background-color: blue;
+}
+</style>
--- src/QuickSort.js
+++ src/QuickSort.js
@@ -1,16 +1,6 @@
-var data = {
-    pic: [1,2,31,3,12,21,23,32,6,5,67,4,2,8,5]
-};
-
-var vm = new Vue({
-    el: '#app',
-    data: data
-});
-
-var arr = [1,2,31,3,12,21,23,32,6,5,67,4,2,8,5];
 var results = [];
 
-function saveState(arr, left, right) {
+function _saveState(arr, left, right) {
     var state = [];
     for(var v in arr) {
         state.push(arr[v]);
@@ -19,15 +9,15 @@ function saveState(arr, left, right) {
     results.push([state, left, right]);
 }
 
-function swap(arr, left, right) {
+function _swap(arr, left, right) {
     var temp = arr[left];
     arr[left] = arr[right];
     arr[right] = temp;
 
-    saveState(arr, left, right);
+    _saveState(arr, left, right);
 }
 
-function quickSort(arr, head, end) {
+export function quickSort(arr, head, end) {
     if(typeof head == "undefined") head = 0;
     if(typeof end == "undefined") end = arr.length - 1;
 
@@ -38,24 +28,13 @@ function quickSort(arr, head, end) {
 
     for(var v = head; v < end; v++) {
         if(arr[v] < pivot) {
-            swap(arr, startIndex++, v);
+            _swap(arr, startIndex++, v);
         }
     }
-    swap(arr, startIndex, end);
+    _swap(arr, startIndex, end);
 
     if(startIndex > head) quickSort(arr, head, startIndex - 1);
     if(startIndex < end) quickSort(arr, startIndex + 1, end);
 
-    return;
-}
-
-
-quickSort(arr);
-
-for(var x in results) {
-    (function(x) {
-        setTimeout(function() {
-            data.pic = results[x];
-        }, x * 500);
-    })(x);
+    return results;
 }
