ad303b7d632d654e6ce04c38156046a47ca072d6
stonexer <shitianxin@gmail.com>
2016-02-03 17:48:58 +0800

    Go Button & SetInterval

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <template>
     <div id="app">
-        <div class="item" v-for="col in pic[0]" track-by="$index">
-            <div class="column" 
-                v-bind:class="{ 'beSwapedColumn': $index == pic[1], 'swapColumn': $index == pic[2] }"
+        <button class="control" v-on:click="go">Go</button>
+        <div class="item" v-for="col in arr[0]" track-by="$index">
+            <div class="item-column" 
+                v-bind:class="{ 'beSwapedColumn': $index == arr[1], 'swapColumn': $index == arr[2] }"
                 v-bind:style="{height: col * 10 + 'px'}">
-             </div>
-             <div class="num">
-                {{col}}
-             </div>
+            </div>
+            <div class="item-num">{{col}}</div>
         </div>
     </div>
 </template>
@@ -15,39 +14,61 @@
 <script>
 import { quickSort } from './QuickSort'
 
-var data = {
-    pic: [1,2,31,3,12,21,23,19,6,5,17,4,2,8,5]
+var go = function () {
+    let results = quickSort(arr);
+    let ticks = setInterval(function() {
+        if(results.length) {
+            data.arr = results.shift();
+        } else {
+            clearInterval(ticks);
+        }
+    }, 100);
+}
+
+let arr = [1,3,12,21,19,6,17,4,2,8,5];
+let data = {
+    arr: [arr]
 };
 
 export default {
     data () {
       return data;
+    },
+    methods: {
+        go: go
     }
 };
 
-var results = quickSort(data.pic);
+go();
 
-for(var x in results) {
-    (function(x) {
-        setTimeout(function() {
-            data.pic = results[x];
-        }, x * 500);
-    })(x);
-}
+// for(let x in results) {
+//     (function(x) {
+//         setTimeout(function() {
+//             data.pic = results[x];
+//         }, x * 500);
+//     })(x);
+// }
 
 </script>
 
 <style>
+.control {
+    position: absolute;
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    font-size: 20px;
+}
 .item {
     display: inline-block;
     width: 20px;
     height: 10px;
     margin-right: 5px;
 }
-.column {
+.item-column {
     background-color: green;
 }
-.item .num {
+.item .item-num {
     text-align: center;
 }
 .item .beSwapedColumn {
